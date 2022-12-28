26cfc38cf36d7b2666d5f4ea1c216eb42552263d
Victor <jotinhagamer4@gmail.com>
2022-12-28 00:36:22 -0300

    game

--- game/src/components/Game.js
+++ game/src/components/Game.js
@@ -3,8 +3,30 @@ import './Game.css'
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <button onClick={verifyLetter}>prox stage, finalizar</button>
+    <div className="game">
+        <p className="points">Pontuação: 000</p>
+        <h1>
+          Advinhe a palavra: 
+        </h1>
+        <h3 className="tip">Dica sobre a palavra <span>dica....</span></h3>
+        <div className="wordContainer">
+            <span className="letter">A</span>
+            <span className="blankSquare"></span>
+        </div>
+        <div className="letterContainer">
+          <p>Tente advinhar uma letra da palavra:</p>
+          <form>
+            <input type="text" name='letter' maxLength='1' required/>
+
+          <button>Jogar</button>
+          </form>
+        </div>
+        <div className="wrongLettersContainer">
+          <p>
+            Letras já utilizadas: <span>a,</span>
+            <span>b,</span>
+          </p>
+        </div>
     </div>
   )
 }
